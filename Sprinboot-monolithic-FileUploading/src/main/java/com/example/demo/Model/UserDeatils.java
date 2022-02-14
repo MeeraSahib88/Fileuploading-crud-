d1b61f28a038410ad63b4e1b080507914edad63c
MeeraSahib88 <84899882+MeeraSahib88@users.noreply.github.com>
2022-02-14 11:04:49 +0530

    Update UserDeatils.java

--- Sprinboot-monolithic-FileUploading/src/main/java/com/example/demo/Model/UserDeatils.java
+++ Sprinboot-monolithic-FileUploading/src/main/java/com/example/demo/Model/UserDeatils.java
@@ -1,20 +1,18 @@
 package com.example.demo.Model;
 
 import java.math.BigDecimal;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data@Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -25,16 +23,11 @@ public class UserDeatils {
 	@GenericGenerator(name = "uuid-gen", strategy = "uuid2")
 	@GeneratedValue(generator = "uuid-gen")
 	private String Id;
-	
 	private String name;
-	
 	private BigDecimal phonenumber;
-	
 	private String email;
-	
 	@Lob
 	@Type(type="org.hibernate.type.BinaryType")
 	private byte[] itemImage;
-	
 
 }
